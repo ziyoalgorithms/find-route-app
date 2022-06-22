e4f57e34124dc8dcc42324ae09f7d34c3757699b
Ziyodullokhon <studiovisual0077@gmail.com>
2022-06-22 06:06:15 -0400

    Bootstrap

--- src/travel/urls.py
+++ src/travel/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home)
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- src/travel/views.py
+++ src/travel/views.py
@@ -1,6 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
 def home(request):
+    title = "bosh sahifa"
     name = 'Ziyodullokhon'
-    return render(request, 'home.html', {'name': name})
+
+    content = {
+        "title": title,
+        "name": name,
+    }
+
+    return render(request, 'home.html', content)
+
+
+
+def about(request):
+    title = 'about'
+    name = 'Biz haqimizda'
+
+    content = {
+        "title": title,
+        "name": name,
+    }
+
+    return render(request, 'about.html', content)
