68c1cfb164a35ac387fe93322b91843056b6c0e8
Ziyodullokhon <studiovisual0077@gmail.com>
2022-07-17 08:07:31 -0400

    Deploy 16

--- travel/settings.py
+++ travel/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email setting
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
