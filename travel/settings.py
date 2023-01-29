79ea0a0f33453f58fac605fdf1d2c290bb9cebbb
ziyoalgorithms <ziyodullokhonanvarov@gmail.com>
2023-01-29 12:07:19 +0500

    Deploy 14

--- travel/settings.py
+++ travel/settings.py
@@ -149,10 +149,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email setting
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studiovisual0077@gmail.com'
-EMAIL_HOST_PASSWORD = 'ackkfyihgqvoswsl'
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 prod_db = dj_database_url.config(conn_max_age=500)
